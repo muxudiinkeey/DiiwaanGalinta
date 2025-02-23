fb94a49bd78ef3d8090eab899e2b9e48ab422356
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-02-24 00:11:44 +0100

    ok

--- diiwaan/src/app/liiska-diiwaanka/liiska-diiwaanka.component.ts
+++ diiwaan/src/app/liiska-diiwaanka/liiska-diiwaanka.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { MareentaService } from './mareenta.service';
+import { Diiwaan } from '../diiwaan';
 
 @Component({
   selector: 'app-liiska-diiwaanka',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './liiska-diiwaanka.component.html',
   styleUrl: './liiska-diiwaanka.component.css'
 })
-export class LiiskaDiiwaankaComponent {
+export class LiiskaDiiwaankaComponent implements OnInit {
+  
+diiwaanka =[];
+diiwaan : Diiwaan[]= []
+  mareentaService = inject(MareentaService )
+
+
+  ngOnInit(): void {
+      this.getLiiskaDiiwaanka();
+  }
+
+  getLiiskaDiiwaanka(){
+    return this.mareentaService.getdiiwaanka().subscribe((diiwaan)=> {
+    this.diiwaan= this.diiwaan
+    })
+  }
 
 }
--- diiwaan/src/app/liiska-diiwaanka/mareenta.service.ts
+++ diiwaan/src/app/liiska-diiwaanka/mareenta.service.ts
@@ -1,11 +1,21 @@
 import { inject, Injectable } from '@angular/core';
+import { Diiwaan } from '../diiwaan';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MareentaService {
 
-  http = inject (MareentaService);
+  http = inject (HttpClient);
+  
+  private url = 'http://localhost:3000/diiwaanka';
 
+
+  diiwaanka : Diiwaan[]= [];
+
+  getdiiwaanka(){
+    return this.http.get(this.url);
+  }
   
 }
