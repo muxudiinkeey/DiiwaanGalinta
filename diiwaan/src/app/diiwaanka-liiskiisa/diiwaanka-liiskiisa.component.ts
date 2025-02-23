1c25578f4c9ece22345874f19ccf5a7796f343ab
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-02-23 18:06:58 +0100

    wargalin ugu horaysay ee gethub waa ok

---
+++ diiwaan/src/app/diiwaanka-liiskiisa/diiwaanka-liiskiisa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diiwaanka-liiskiisa',
+  imports: [],
+  templateUrl: './diiwaanka-liiskiisa.component.html',
+  styleUrl: './diiwaanka-liiskiisa.component.css'
+})
+export class DiiwaankaLiiskiisaComponent {
+
+}
