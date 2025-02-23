59b6cf71affbd4d131650f6b22b2dddf7e6d2970
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-02-23 20:30:02 +0100

    ok

--- diiwaan/src/app/app.routes.ts
+++ diiwaan/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { KudarDiiwaankaComponent } from './kudar-diiwaanka/kudar-diiwaanka.component';
+import { LiiskaDiiwaankaComponent } from './liiska-diiwaanka/liiska-diiwaanka.component';
+import { WaxkabadalDiiwaankaComponent } from './waxkabadal-diiwaanka/waxkabadal-diiwaanka.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'liikaDiiwaanka' },
+    { path: 'liiskaDiiwaanka', component: LiiskaDiiwaankaComponent },
+    { path: 'kudar', component:KudarDiiwaankaComponent },
+    { path: 'waxkabalkaDiiwaanka', component: WaxkabadalDiiwaankaComponent},
+    
+];
