349f60481dabef838a7be9810757b2a003387167
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-03-26 20:36:49 +0100

    ok

---
+++ diiwaan/src/app/page-not-founnd/page-not-founnd.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-founnd',
+  imports: [],
+  templateUrl: './page-not-founnd.component.html',
+  styleUrl: './page-not-founnd.component.css'
+})
+export class PageNotFounndComponent {
+
+}
